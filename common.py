87c3c08701c3c15d213e5a3ad66619ee3cc6426e
Aditya Singh <baddrink.887@gmail.com>
2020-04-17 13:10:18 +0530

    Adi

--- common.py
+++ common.py
@@ -5,7 +5,7 @@ import logging
 # mode = os.getenv("MODE")
 # TELEGRAM_ACCESS_TOKEN = os.getenv("TOKEN")
 
-mode = "dev"
+mode = "prod"
 TELEGRAM_ACCESS_TOKEN = "938130802:AAEnlVu9eg8onfx34O-D7CgKL4OXVhAESh0"
 HEROKU_APP_NAME = "libgens-bot"
 LIBGEN_DOMAIN = "https://libgen.is/"
